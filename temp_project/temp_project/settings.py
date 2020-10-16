2fab10c306b2e86c8141b602b9ef9109194033d6
Win-Htet-Aung <whaung1998@gmail.com>
2020-10-16 19:07:33 +0630

    mysql setup

--- temp_project/temp_project/settings.py
+++ temp_project/temp_project/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'temp_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'winhtetaung.mysql.pythonanywhere-services.com',
+        'USERNAME': 'winhtetaung',
+        'PASSWORD': 'mydbisintheair',
+        'NAME': 'profour',
     }
 }
 
